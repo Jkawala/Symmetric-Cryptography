d7bb07c4c06963c8390f4954f74d9c363bb6c083
Jkawala <91265409+Jkawala@users.noreply.github.com>
2026-01-04 20:47:22 +0000

    Add files via upload

---
+++ add_round_key_b67b9a529ae739156107a74b14adde98.py
@@ -0,0 +1,17 @@
+def add_round_key(s, k):
+    return [[s[r][c] ^ k[r][c] for c in range(4)] for r in range(4)]
+state = [
+    [206, 243, 61, 34],
+    [171, 11, 93, 31],
+    [16, 200, 91, 108],
+    [150, 3, 194, 51],
+]
+
+round_key = [
+    [173, 129, 68, 82],
+    [223, 100, 38, 109],
+    [32, 189, 53, 8],
+    [253, 48, 187, 78],
+]
+
+print(add_round_key(state, round_key))
