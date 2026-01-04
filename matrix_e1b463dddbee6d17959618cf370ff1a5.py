44638a58036c4d68967a9287e544573f555d16e8
Jkawala <91265409+Jkawala@users.noreply.github.com>
2026-01-04 20:44:25 +0000

    Add files via upload

---
+++ matrix_e1b463dddbee6d17959618cf370ff1a5.py
@@ -0,0 +1,13 @@
+def matrix2bytes(matrix):
+    """ Converts a 4x4 matrix into a 16-byte array. """
+    return bytes([b for row in matrix for b in row])
+
+matrix = [
+    [99, 114, 121, 112],
+    [116, 111, 123, 105],
+    [110, 109, 97, 116],
+    [114, 105, 120, 125],
+]
+
+print(matrix2bytes(matrix))
+
